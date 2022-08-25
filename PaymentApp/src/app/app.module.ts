13507b8049ef987696d8610e237eea0c6aff4b9b
Ahmed <mr.incognito022@gmail.com>
2022-08-25 15:54:12 -0700

    Component and service class is added

--- PaymentApp/src/app/app.module.ts
+++ PaymentApp/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PaymentDetailComponent } from './payment-detail/payment-detail.component';
+import { PaymentDetailFormComponent } from './payment-detail/payment-detail-form/payment-detail-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentDetailComponent,
+    PaymentDetailFormComponent,
   ],
   imports: [
     BrowserModule
